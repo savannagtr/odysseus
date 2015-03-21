7ba92afe6748119331580fb100090d2f97d0ea0b
hannahhannah <cerberusjump27@rocketmail.com>
2015-03-20 20:13:29 -0700

    Initial commit

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult Story()
+        {
+            return View();
+        }
+
+        public ActionResult Objective()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+    }
+          
+    }
+
